6158eb262325268c6f5a2509c0d85e5ce02e3ca7
unknown <j@jvniorrr.com>
2021-07-06 23:25:17 -0700

    Update main.py
    - Added new commands, still need better error handling though.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord, logging, json, os
+from discord import user
 from dotenv import load_dotenv
 from discord.ext import commands
 from discord import Client
@@ -16,7 +17,7 @@ DEV_ID = os.getenv("DEV_ID")
 
 
 logging.basicConfig(datefmt="%I:%M:%S", format=f"[%(levelname)s] [%(asctime)s.%(msecs)03d] - > %(message)s",level=logging.INFO)
-bot = commands.Bot(command_prefix='-', heartbeat_timeout=30, self_bot=False)
+bot = commands.Bot(command_prefix='!', heartbeat_timeout=30, self_bot=False)
 bot.remove_command('help')
 
 
@@ -27,26 +28,111 @@ async def on_ready():
 
 
 # COMMANDS
+# @bot.command()
+# @discord.ext.commands.core.has_permissions(administrator=True)
+# async def entry(ctx, member:discord.Member, serverid:int):
+#     # create a connection to the DB
+#     database = databaseMongo.Database(MONGO_USER, MONGO_PASSWORD)
+
+#     # retrieve info from hyonix
+#     hyonix_api = hyonix.HyonixAPI(HYONIX_TOKEN)
+    
+#     serverInformation = hyonix_api.getServerDetails(serverid=serverid)
+
+#     #  make sure there is a server with that ID passed
+#     if serverInformation == None:
+#         logging.info("Server ID is not valid, please provide a valid ID.")
+#         await ctx.channel.send("Invalid Server ID, please provide a valid Server ID.")
+#         return
+
+
+#     # check if user is already stored in db
+#     current_user = database.checkCurrentUser(member.id)
+
+#     # if current user already stored dont enter a new one
+#     if current_user == True:
+#         # send msg to client & just append to their current entry
+#         logging.info("A User with that ID is already stored. Updating their current servers.")
+#         updated = hyonix_api.updateUser(member.id, serverInformation)
+#         if updated == True:
+#             await ctx.channel.send("Successfully updated the Users current / active servers.")
+#             return
+#         else:
+#             await ctx.channel.send("Unknown error updating the users information. Making Developer aware.")
+#             developer = bot.get_user(DEV_ID)
+#             await developer.create_dm().send(f"Error occured updating User: {member.mention}")
+#             return
+
+#     # if ths user is not in the DB
+#     else:
+#         # assure that the entry was entered to DB
+#         entered = database.entry(member.name, member.id, serverInformation)
+#         if entered == True:
+#             logging.info("Successfully added a new entry to the DB.")
+#             await ctx.channel.send("Successfully added a new entry to the database.")
+#             return
+        
+#         #  if the entry was unsuccessfull
+#         else:
+#             await ctx.channel.send("Unknown error creating a new users database entry. Making Developer aware.")
+#             developer = bot.get_user(DEV_ID)
+#             await developer.create_dm().send(f"Error occured creating a new entry for User: {member.mention}")
+#             return
+
+
 @bot.command()
 @discord.ext.commands.core.has_permissions(administrator=True)
-async def entry(ctx, member:discord.Member, serverid:int):
+async def adduser(ctx):
+
+    # Retrieve the User associated with said server
+    await ctx.channel.send("What user is being added?")
+    # assure that the response is a user being provided.
+    def check(response):
+        if response.mentions[0] and response.author == ctx.author:
+            # print("True")
+            return True
+        else:
+            return False
+    userResponse = await bot.wait_for('message', check=check)
+    member = userResponse.mentions[0]
+
+
+    #  retrieve the SERVER ID
+    def checkserver(response_):
+        if int(response_.content) and response_.author == ctx.author:
+            return True
+        else:
+            return False
+    await ctx.channel.send("What is the Servers ID?")
+    serverID = await bot.wait_for('message', check=checkserver)
+
+
+    # retrieve the next due date
+    await ctx.channel.send("When is the next due date? *(format: mm-dd-yyyy)*")
+    def checkDate(response):
+        return response.author == ctx.author
+    due_date_r = await bot.wait_for('message', check=checkDate)
+
+    due_date = due_date_r.content
+
+    
     # create a connection to the DB
     database = databaseMongo.Database(MONGO_USER, MONGO_PASSWORD)
 
     # retrieve info from hyonix
     hyonix_api = hyonix.HyonixAPI(HYONIX_TOKEN)
-    
-    serverInformation = hyonix_api.getServerDetails(serverid=serverid)
 
-    #  make sure there is a server with that ID passed
+    serverInformation = hyonix_api.getServerDetails(serverid=serverID.content)
+
+
+    current_user = database.checkCurrentUser(member.id)
+    
+
+
     if serverInformation == None:
         logging.info("Server ID is not valid, please provide a valid ID.")
         await ctx.channel.send("Invalid Server ID, please provide a valid Server ID.")
         return
-
-
-    # check if user is already stored in db
-    current_user = database.checkCurrentUser(member.id)
 
     # if current user already stored dont enter a new one
     if current_user == True:
@@ -65,7 +151,7 @@ async def entry(ctx, member:discord.Member, serverid:int):
     # if ths user is not in the DB
     else:
         # assure that the entry was entered to DB
-        entered = database.entry(member.name. member.id, serverInformation)
+        entered = database.entry(member.name, member.id, serverInformation, due_date)
         if entered == True:
             logging.info("Successfully added a new entry to the DB.")
             await ctx.channel.send("Successfully added a new entry to the database.")
@@ -78,36 +164,164 @@ async def entry(ctx, member:discord.Member, serverid:int):
             await developer.create_dm().send(f"Error occured creating a new entry for User: {member.mention}")
             return
 
+
 @bot.command()
 @discord.ext.commands.core.has_permissions(administrator=True)
-async def removeEntry(ctx, member:discord.Member):
-    """Method to remove a user from the database
-    
-    Parameters
-    -----------
-    member : discord.Member
-        - discord member that is being removed from the DB
-    """
+async def removeuser(ctx):
+    # Retrieve the User associated with said server
+    await ctx.channel.send("What user is being removed?")
+    # assure that the response is a user being provided.
+    def check(response):
+        if response.mentions[0] and response.author == ctx.author:
+            # print("True")
+            return True
+        else:
+            return False
+    userResponse = await bot.wait_for('message', check=check)
+    member = userResponse.mentions[0]
 
+    # check if the user is stored
     database = databaseMongo.Database(MONGO_USER, MONGO_PASSWORD)
-    
-    removed = False
-    # check if the user is stored in the DB
+
     current_user = database.checkCurrentUser(member.id)
 
-    if current_user == True:
-        removed = database.removeUser(member.id)
-        if removed == True:
-            await ctx.channel.send(f"{member.name} was successfully removed from the database.")
-            return
-        else:
-            await ctx.channel.send("Unknown error occured removing User from the database. Making Developer aware.")
-            developer = bot.get_user(DEV_ID)
-            await developer.create_dm().send(f"Error occured removing User: {member.mention} from the database.")
-            return
-    else:
-        await ctx.channel.send(f"{member.name} is not stored in the database. Provide a member that has an active sub.")
+    if current_user == False:
+        await ctx.channel.send("That user is not stored in the database. Please provide a valid user.")
         return
+
+    #  retrieve the users information, see if the have > 1 server associated
+    userInformation = database.returnUserInformation(member.id)
+
+    if userInformation == None:
+        await ctx.channel.send("Unknown error occured removing server from User from the database. Making Developer aware.")
+        developer = bot.get_user(DEV_ID)
+        await developer.create_dm().send(f"Error occured retrieving server information for User: {member.mention} database.")
+        return
+
+
+
+    elif userInformation != None:
+        if len(userInformation["servers"]) > 1:
+            #  ask the admin what server they want removed
+            await ctx.channel.send("What server IP would you like removed?")
+
+            def checkServers(response):
+                return response.author == ctx.author
+            
+            userResponse = await bot.wait_for("message", check=checkServers)
+
+            serverIP = userResponse.content
+
+            for server in userInformation["servers"]:
+                if serverIP == server["server_ip"]:
+                    removed = database.removeServer(discord_id=member.id, server_ip=serverIP)
+                    #  check if it was removed succesfully
+                    if removed == True:
+                        await ctx.channel.send(f"Succesfully removed Server IP: {serverIP}")
+                        return
+                    else:
+                        await ctx.channel.send("Unknown error occured removing server from User from the database. Making Developer aware.")
+                        developer = bot.get_user(DEV_ID)
+                        await developer.create_dm().send(f"Error occured removing User: {member.mention} server from the database.")
+                        return
+
+            # return a msg if that ip isnt found in loop above
+            await ctx.channel.send(f"Provide a valid server IP for user: {member.mention}")
+                        
+        else:
+            removed = database.removeUser(member.id)
+            if removed == True:
+                await ctx.channel.send("Successfully removed user from the database.")
+            else:
+                await ctx.channel.send("Unknown error occured removing User from the database. Making Developer aware.")
+                developer = bot.get_user(DEV_ID)
+                await developer.create_dm().send(f"Error occured removing User: {member.mention} from the database.")
+                return
+    return
+
+
+@bot.command()
+@discord.ext.commands.core.has_permissions(administrator=True)
+async def renew(ctx):
+    # Retrieve the User associated with said server
+    await ctx.channel.send("What user is being renewed?")
+    def check(response):
+        if response.mentions[0] and response.author == ctx.author:
+            return True
+        else:
+            return False
+    userResponse = await bot.wait_for('message', check=check)
+    member = userResponse.mentions[0]
+
+    # check if the user is stored
+    database = databaseMongo.Database(MONGO_USER, MONGO_PASSWORD)
+
+    current_user = database.checkCurrentUser(member.id)
+
+    if current_user == False:
+        await ctx.channel.send("That user is not stored in the database. Please provide a valid user.")
+        return
+    
+    await ctx.channel.send("What is the new renewal date? *(format: mm-dd-yyyy)*")
+
+    def checkDate(response):
+        if len(response.content.split("-")) == 3 and response.author == ctx.author:
+            return True
+        else:
+            return False
+
+    dateResponse = await bot.wait_for("message", check=checkDate)
+
+    # update the database
+    renewedStatus = database.renewalDate(member.id, dateResponse.content)
+
+    if renewedStatus == False:
+        await ctx.channel.send("Unknown error updating the users renewal date. Making Developer aware.")
+        developer = bot.get_user(DEV_ID)
+        await developer.create_dm().send(f"Error occured updating the renewal date for User: {member.mention}")
+        return
+    
+    else:
+        await ctx.channel.send("Successfully update the next due date for user.")
+        return
+            
+
+    
+
+
+
+
+
+# @bot.command()
+# @discord.ext.commands.core.has_permissions(administrator=True)
+# async def removeEntry(ctx, member:discord.Member):
+#     """Method to remove a user from the database
+    
+#     Parameters
+#     -----------
+#     member : discord.Member
+#         - discord member that is being removed from the DB
+#     """
+
+#     database = databaseMongo.Database(MONGO_USER, MONGO_PASSWORD)
+    
+#     removed = False
+#     # check if the user is stored in the DB
+#     current_user = database.checkCurrentUser(member.id)
+
+#     if current_user == True:
+#         removed = database.removeUser(member.id)
+#         if removed == True:
+#             await ctx.channel.send(f"{member.name} was successfully removed from the database.")
+#             return
+#         else:
+#             await ctx.channel.send("Unknown error occured removing User from the database. Making Developer aware.")
+#             developer = bot.get_user(DEV_ID)
+#             await developer.create_dm().send(f"Error occured removing User: {member.mention} from the database.")
+#             return
+#     else:
+#         await ctx.channel.send(f"{member.name} is not stored in the database. Provide a member that has an active sub.")
+#         return
 
 
 
@@ -129,6 +343,11 @@ async def restartServer(ctx, server_ip:str):
     if current_user == True:
         # retrieve the users current information
         userInformation = database.returnUserInformation(ctx.author.id)
+
+        if userInformation == None:
+            return
+
+
 
         for server in userInformation["servers"]:
             if server["server_ip"] == server_ip:
@@ -155,18 +374,23 @@ async def resetPassword(ctx, server_ip:str):
     if current_user == True:
         # retrieve the users current information
         userInformation = database.returnUserInformation(ctx.author.id)
+
+        if userInformation == None:
+            return
+
         for server in userInformation["servers"]:
             if server["server_ip"] == server_ip:
-                password, reset = hyonix_api.resetPassword(server["server_id"])
+                # print(server)
+                password, reset = hyonix_api.resetPassword(server['server_id'])
                 if reset == True:
                     await ctx.channel.send(f"Successfully reset the password for Server: {server_ip}")
 
                     # create an embed with the new info
                     hex_color = int(f"0xF9004D", 16)
                     embed = discord.Embed(color=hex_color, title="Renowned Servers", author="Renowned Servers")
-                    embed.add_field("IP Address", server["server_ip"], False)
-                    embed.add_field("User", "Administrator", False)
-                    embed.add_field("Password", password, False)
+                    embed.add_field(name="IP Address", value=f"{server['server_ip']}", inline=False)
+                    embed.add_field(name="User", value="Administrator", inline=False)
+                    embed.add_field(name="Password", value=f"||`{password}`||", inline=False)
                     await ctx.channel.send(embed=embed)
     
                     return
@@ -175,3 +399,5 @@ async def resetPassword(ctx, server_ip:str):
 
 
 
+if __name__ == "__main__":
+    bot.run(DISCORD_TOKEN)
